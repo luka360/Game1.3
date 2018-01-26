133b2270d6641a1205d58ce08534b87c26d099af
luka360 <35566250+luka360@users.noreply.github.com>
2018-01-26 13:15:51 +0100

    First commit

---
+++ Assets/MyScripts/CameraControllerRPG.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControllerRPG : MonoBehaviour {
+
+    [SerializeField] Transform target;
+    [SerializeField] Vector3 offset;
+    [SerializeField] float pitch = 2f;
+    [SerializeField] float zoomSpeed = 4f;
+    [SerializeField] float minZoom = 5f;
+    [SerializeField] float maxZoom = 15f;
+    [SerializeField] float yawSpeed = 100f;
+
+    private float currentZoom = 10f;
+    private float currentYaw = 0f;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void Update()
+    {
+        currentZoom -= Input.GetAxis("Mouse ScrollWheel") * zoomSpeed;
+        currentZoom = Mathf.Clamp(currentZoom, minZoom, maxZoom);
+
+        currentYaw -= Input.GetAxis("Horizontal") * yawSpeed * Time.deltaTime;
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
+        transform.position = target.position - offset * currentZoom;
+        transform.LookAt(target.position + Vector3.up * pitch);
+
+        transform.RotateAround(target.position, Vector3.up, currentYaw);
+	}
+}
---
+++ Assets/MyScripts/PlayerContoller.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerContoller : MonoBehaviour {
+
+    [SerializeField] LayerMask movementMask;
+    [SerializeField] Interactable focus;
+
+    private Camera cam;
+    PlayerMotor motor;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        motor = GetComponent<PlayerMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100, movementMask))
+            {
+                motor.MoveToPoint(hit.point);
+
+                // Stop focusing any objects
+                RemoveFocus();
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                // Check if we hit an interactible, and if we did set it as our focus
+                Interactable interactable = hit.collider.GetComponent<Interactable>();
+                if (interactable != null)
+                {
+                    SetFocus(interactable);
+                }
+                
+            }
+        }
+    }
+
+    void RemoveFocus()
+    {
+        focus = null;
+    }
+
+    void SetFocus(Interactable newFocus)
+    {
+        focus = newFocus;
+    }
+}
