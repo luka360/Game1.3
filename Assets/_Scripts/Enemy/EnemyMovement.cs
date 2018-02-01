89aa8325a5c8cbaaca10a2fa2f4000499967d80c
luka360 <35566250+luka360@users.noreply.github.com>
2018-02-01 23:59:53 +0100

    added enemy and enemy basic locomotion

---
+++ Assets/_Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public Transform player;
+
+	NavMeshAgent navAgent;
+	private Animator _animator;
+
+
+	void Awake(){
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+		navAgent = GetComponent<NavMeshAgent> ();
+		_animator = GetComponent<Animator> ();
+	}
+		
+	// Update is called once per frame
+	void Update () {
+
+		if (Vector3.Distance(player.position, transform.position) > 4 && Vector3.Distance(player.position, transform.position) < 10) {
+			navAgent.SetDestination (player.position);
+			_animator.SetBool ("Move", true);
+		} else {
+			navAgent.isStopped = true;
+			_animator.SetBool ("Move", false);
+		}
+
+	}
+}
---
+++ Assets/_Scripts/Player/HitCollider.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitCollider : MonoBehaviour {
+
+	public float kickForce = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("I hit " + other.gameObject.name + " with " + this.gameObject.name);
+		Rigidbody body = other.attachedRigidbody;
+		if (body != null && !body.isKinematic) {
+			body.velocity = other.transform.position * kickForce;
+		}
+	}
+}
